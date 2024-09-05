b15401ee58f3319396240c62066289d9491347da
Udit Tripathi <114675604+Udit-Tripathi@users.noreply.github.com>
2024-09-05 20:24:12 +0100

    Delete MyBlog/blog directory

--- MyBlog/blog/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-import environ
-
-env = environ.Env()
-environ.Env.read_env()
-
-print("SECRET_KEY:", env('SECRET_KEY', default='not_found'))
-
-# Create your tests here.
