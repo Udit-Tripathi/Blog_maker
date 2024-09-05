a260faea19c2f6fe40c13bf5c6f6ecbf645efcbd
Udit Tripathi <114675604+Udit-Tripathi@users.noreply.github.com>
2024-09-05 20:23:26 +0100

    Update entrypoint.sh

--- MyBlog/entrypoint.sh
+++ MyBlog/entrypoint.sh
@@ -6,7 +6,7 @@ python manage.py migrate
 # Collect static files
 python manage.py collectstatic --noinput
 
-# Start the server using gunicorn on port 8080
+# Start the server 
 exec gunicorn --bind 0.0.0.0:8080 MyBlog.wsgi
 
 
